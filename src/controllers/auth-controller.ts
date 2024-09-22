71e0f14f8d59a93aceadd8577555cd5733bf51a4
Daniel <daniel.40.pompa@gmail.com>
2024-09-23 01:09:43 +0200

    feat: add token model and six-digit token generation
    - Created a Mongoose model for tokens with an expiration of 10 minutes.
    - Implemented a function to generate a six-digit token.
    - Updated `AuthController` to generate and save a token when a new account is created.
    - Ensured unique token assignment for new users during account creation.

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,22 +1,28 @@
 import type { Request, Response } from 'express';
-import { User } from '../models';
-import { hashPassword } from '../utils/auth';
+import { Token, User } from '../models';
+import { generateToken, hashPassword } from '../utils';
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
       const { email, password } = req.body;
-      /** Avoid duplicates in the database */
+      // Check if the user already exists
       const userExists = await User.findOne({ email });
       if (userExists) {
         const error = new Error('Ya existe una cuenta con ese correo electrónico.');
         return res.status(409).json({ message: error.message });
       }
-      /** Create a new user */
+      // Create a new user and hash the password
       const user = new User(req.body);
-      /** Password encryption using bcrypt */
       user.password = await hashPassword(password);
-      await user.save();
+      // Generate a token for the new user
+      const tokenValue = generateToken();
+      const token = new Token({
+        token: tokenValue,
+        user: user.id,
+      });
+      // Save the user and the token in the database
+      await Promise.allSettled([user.save(), token.save()]);
       res.status(201).json({
         message:
           'Cuenta creada con éxito. Revisa tu correo electrónico para confirmarla.',
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,8 @@
 export { default as Project } from './Project';
 export { default as Task } from './Task';
 export { default as User } from './User';
+export { default as Token } from './Token';
 export type { IProject } from './Project';
 export type { ITask } from './Task';
 export type { IUser } from './User';
+export type { IToken } from './Token';
