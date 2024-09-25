c604daab7d2f0d6baeef733f4d8a3b3557f7721d
Daniel <daniel.40.pompa@gmail.com>
2024-09-25 23:26:06 +0200

    feat: update confirmation email with link
    Added a confirmation link to the account verification email sent to users.

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -16,7 +16,9 @@ export class AuthEmail {
       html: `
         <h1>Bienvenido a Daem</h1>
         <p>Hola ${user.name}, confirma tu cuenta en Daem.</p>
-        <p>Haz clic en el siguiente enlace: <a href="#">confirmar cuenta</a></p>
+        <p>Haz clic en el siguiente enlace: 
+          <a href="${process.env.REACT_APP_FRONTEND_URL}/auth/confirm-account">confirmar cuenta</a>
+        </p>
         <p>Tu código de verificación de 6 dígitos es: <b>${user.token}</b></p>
         <p>Este código expirará en 10 minutos.</p>
         <p>Si no creaste esta cuenta, ignora este correo.</p>
