596f10f8064247eb79ec56e40ac861ed1cb02f89
Daniel <daniel.40.pompa@gmail.com>
2024-10-29 00:30:18 +0100

    docs: enhance API documentation details
    Updated the Swagger configuration to provide more comprehensive documentation for the API. The following changes were made:
    
    - Added a description to the API documentation for clarity.
    - Included server URLs for both development and production.

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -3,6 +3,12 @@ import swaggerJSDoc from 'swagger-jsdoc';
 const options = {
   swaggerDefinition: {
     openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description:
+        'Comprehensive documentation for the API, detailing endpoints, parameters, and responses.',
+    },
     tags: [
       {
         name: 'Auth',
@@ -13,10 +19,16 @@ const options = {
         description: 'API operations related to projects.',
       },
     ],
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-    },
+    servers: [
+      {
+        url: 'http://localhost:4000',
+        description: 'Development server',
+      },
+      {
+        url: 'https://project-management-backend-kvhy.onrender.com/docs',
+        description: 'Production server',
+      },
+    ],
   },
   apis: ['./src/routes/**/*.ts'],
 };
